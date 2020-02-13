fdc8ca3bd7b249ff6f3e9ffaf99722357901b414
Zeh Fernando <zeh@work.co>
2020-02-13 17:18:52 -0500

    App: dragging an image into the app loads it as the door texture

--- src/ts/components/App/index.tsx
+++ src/ts/components/App/index.tsx
@@ -7,22 +7,49 @@ import HypeBox from "../HypeBox";
 
 import styles from "./styles";
 
-export default class App extends React.Component {
+interface IState {
+	currentTexture: HTMLImageElement | undefined;
+}
+
+export default class App extends React.Component<any, IState> {
 	public constructor(props: any) {
 		super(props);
+
+		this.handleDropFiles = this.handleDropFiles.bind(this);
+
+		this.state = {
+			currentTexture: undefined,
+		};
 	}
 
 	public render(): JSX.Element {
+		const { currentTexture } = this.state;
+
 		return (
 			<div className={styles.main}>
 				<Header />
 				<Canvas>
 					<ambientLight />
 					<pointLight position={[10, 10, 10]} />
-					<HypeBox />
+					<HypeBox image={currentTexture} />
 				</Canvas>
-				<DropTarget />
+				<DropTarget onDropFiles={this.handleDropFiles} />
 			</div>
 		);
+	}
+
+	private handleDropFiles(files: File[]): void {
+		const file = files.find((f) => f.name.endsWith(".png") || f.name.endsWith(".jpg"));
+		if (file) {
+			const reader = new FileReader();
+			reader.addEventListener("load", (e) => {
+				if (e.target && typeof e.target.result === "string") {
+					const img = new Image();
+					img.src = e.target.result;
+					this.setState({ currentTexture: img });
+				}
+			});
+			reader.readAsDataURL(file);
+		}
 	}
 }
